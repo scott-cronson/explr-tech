651e87617dc332cb53325f73633bff33eec11762
Scott Cronson <scc@insomniacdesign.com>
2025-01-24 17:52:06 -0500

    Terraform init based on GCP getting-started-tutorial

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance"
+    machine_type = "f1-micro"
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-11"
+        }
+    }
+
+    network_interface {
+        network = google_compute_network.vpc_network.name
+        access_config {
+        }
+    }
+}
